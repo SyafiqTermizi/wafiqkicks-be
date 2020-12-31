4a5617cf7b0f332b35533c90ad4c06306b064d88
syafiq <ahmad.syafiq@jewelpaymentech.com>
2020-12-31 16:24:36 +0800

    Feature: order kicks by time

--- kicks/models.py
+++ kicks/models.py
@@ -4,5 +4,8 @@ from django.db import models
 class Kick(models.Model):
     kick_time = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering  = ["-kick_time"]
+
     def __str__(self) -> str:
         return self.kick_time
--- kicks/views.py
+++ kicks/views.py
@@ -3,9 +3,9 @@ from rest_framework.response import Response
 
 from .models import Kick
 
+
 class KickCountUpView(APIView):
 
     def post(self, request, *args, **kwargs):
         Kick.objects.create()
         return Response(status=201)        
-    
