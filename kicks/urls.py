55833861181b76efd9673585a01fe7ea1593d837
syafiq <ahmad.syafiq@jewelpaymentech.com>
2021-01-01 20:04:23 +0800

    Feature: filter hourly kicks by hour

--- kicks/urls.py
+++ kicks/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import DailyKickSummaryView, KickCountUpView, DailyChartView
+from .views import DailyKickSummaryView, KickCountUpView, DailyChartView, KickDatesView
 
 urlpatterns = (
     path("count-up/", KickCountUpView.as_view(), name="count_up"),
     path("daily-summary/", DailyKickSummaryView.as_view(), name="daily_summary"),
     path("daily-chart/", DailyChartView.as_view(), name="daily_chart"),
+    path("dates/", KickDatesView.as_view(), name="dates"),
 )
--- kicks/views.py
+++ kicks/views.py
@@ -1,4 +1,4 @@
-from datetime import timedelta
+from datetime import datetime, timedelta
 from django.utils import timezone
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -9,6 +9,11 @@ from .utils import get_start_hour
 
 
 class KickCountUpView(APIView):
+    """
+    An endpoint to create new kicks.Kick model. Every POST request to this view
+    will create new row kicks.Kick
+    """
+
     def post(self, request, *args, **kwargs):
         try:
             last_kick_time = Kick.objects.latest().kick_time
@@ -16,6 +21,8 @@ class KickCountUpView(APIView):
             pass
         else:
             delta = timezone.now() - last_kick_time
+
+            # only allow user to update data once every minute
             if delta.total_seconds() < 59:
                 return Response(status=403)
 
@@ -24,9 +31,16 @@ class KickCountUpView(APIView):
 
 
 class DailyKickSummaryView(APIView):
+    """
+    This endpoint will return daily kick summary.
+    The data returned:
+        1. First kick time
+        2. Last kick time
+        3. Total kick for today
+    """
+
     def get(self, request, *args, **kwargs):
         today_kicks = Kick.objects.filter(kick_time__gte=get_start_hour())
-
         today_kick_count = today_kicks.count()
 
         if not today_kick_count:
@@ -41,16 +55,36 @@ class DailyKickSummaryView(APIView):
 
 class DailyChartView(APIView):
     def get(self, request, *args, **kwargs):
-
-        hours = Kick.objects.filter(kick_time__gte=get_start_hour()).datetimes(
-            "kick_time", "hour"
-        )
+        try:
+            parsed_date = datetime.strptime(
+                request.query_params.get("date", ""), "%Y-%m-%d"
+            )
+        except ValueError:
+            hours = Kick.objects.filter(kick_time__gte=get_start_hour()).datetimes(
+                "kick_time", "hour"
+            )
+        else:
+            hours = Kick.objects.filter(
+                kick_time__day=parsed_date.day,
+                kick_time__month=parsed_date.month,
+                kick_time__year=parsed_date.year,
+            ).datetimes("kick_time", "hour")
 
         kicks_per_hour = {}
         for hour in hours:
-
             kicks_per_hour[hour.strftime("%-I %p")] = Kick.objects.filter(
                 kick_time__gte=hour, kick_time__lte=(hour + timedelta(hours=1))
             ).count()
 
         return Response(data=kicks_per_hour, status=200)
+
+
+class KickDatesView(APIView):
+    """
+    Return all the dates the baby has been kicking
+    """
+
+    def get(self, request, *args, **kwargs):
+        days = Kick.objects.dates("kick_time", "day")
+
+        return Response(data=days, status=200)
