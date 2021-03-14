996ed5b6d08049fcd7f63ea91be074082e1c9c55
syafiq_termizi <ahmadsyafiq93@gmail.com>
2021-03-14 13:51:43 +0800

    Feature? ¯\_(ツ)_/¯

--- static/ts/Routes.tsx
+++ static/ts/Routes.tsx
@@ -9,38 +9,42 @@ import { FetalMovementChart } from "./pages/FMC";
 import { Login } from "./pages/Login";
 import { BottomNavbar } from "./components/BottomNavbar";
 
+const AuthenticatedRoutes = () => (
+  <>
+    <Route path="/" exact={true}>
+      <Redirect to="/home" />
+    </Route>
+    <Route path="/home" exact={true}>
+      <Counter />
+    </Route>
+    <Route path="/chart" exact={true}>
+      <Chart />
+    </Route>
+    <Route path="/fmc" exact={true}>
+      <FetalMovementChart />
+    </Route>
+    <BottomNavbar />
+  </>
+);
+
+const UnauthenticatedRoutes = () => (
+  <>
+    <Route path="/" exact={true}>
+      <Redirect to="/login" />
+    </Route>
+    <Route path="/login" exact={true}>
+      <Login />
+    </Route>
+  </>
+);
+
 export const Routes = () => {
   const userToken = useAppSelector((state) => state.user.token);
 
   return (
     <Switch>
-      {userToken && (
-        <>
-          <Route path="/" exact={true}>
-            <Redirect to="/home" />
-          </Route>
-          <Route path="/home" exact={true}>
-            <Counter />
-          </Route>
-          <Route path="/chart" exact={true}>
-            <Chart />
-          </Route>
-          <Route path="/fmc" exact={true}>
-            <FetalMovementChart />
-          </Route>
-          <BottomNavbar />
-        </>
-      )}
-      {!userToken && (
-        <>
-          <Route path="/" exact={true}>
-            <Redirect to="/login" />
-          </Route>
-          <Route path="/login" exact={true}>
-            <Login />
-          </Route>
-        </>
-      )}
+      {userToken && <AuthenticatedRoutes />}
+      {!userToken && <UnauthenticatedRoutes />}
     </Switch>
   );
 };
