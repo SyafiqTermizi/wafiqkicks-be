eaa1625611a5a774a57b45e83d56737729b7489c
syafiq_termizi <ahmadsyafiq93@gmail.com>
2021-03-14 12:46:52 +0800

    Feature: load and save data to localStorage

--- static/ts/store/store.ts
+++ static/ts/store/store.ts
@@ -2,9 +2,26 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import user from "./userSlice";
 
+const preloadedState = {
+  user: {
+    email: localStorage.getItem("email") || "",
+    username: localStorage.getItem("username") || "",
+    token: localStorage.getItem("token") || "",
+  },
+};
+
 export const store = configureStore({
   reducer: { user },
+  preloadedState,
 });
+
+const saveStateToLocalStorage = () => {
+  const userState = store.getState().user;
+  localStorage.setItem("token", userState.token);
+  localStorage.setItem("email", userState.email);
+  localStorage.setItem("username", userState.username);
+};
+store.subscribe(() => saveStateToLocalStorage());
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
