c60f3754dd0e4f2daadf03e1f7f8a43d2178c9f0
syafiq_termizi <ahmadsyafiq93@gmail.com>
2021-05-01 22:48:17 +0800

    Chore: Test FromToDateSerializer

--- preggy/kicks/tests.py
+++ preggy/kicks/tests.py
@@ -1,3 +1,33 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.test.testcases import SimpleTestCase
+from rest_framework import serializers
 
-# Create your tests here.
+from preggy.kicks.serializers import FromToDateSerializer
+
+
+class TestFromToDateSerializer(SimpleTestCase):
+    def test_to_date_required(self):
+        """
+        FromToDateSerializer should raise ValidationError if to_date is
+        not provided
+        """
+        serializer = FromToDateSerializer(
+            data={
+                "from_date": "01-01-2021",
+            }
+        )
+        with self.assertRaises(serializers.ValidationError):
+            serializer.is_valid(raise_exception=True)
+
+    def test_from_date_required(self):
+        """
+        FromToDateSerializer should raise ValidationError if from_date is
+        not provided
+        """
+        serializer = FromToDateSerializer(
+            data={
+                "to_date": "01-01-2021",
+            }
+        )
+        with self.assertRaises(serializers.ValidationError):
+            serializer.is_valid(raise_exception=True)
