af339d78bbe58952e1a81328a00806befdb0e297
syafiq_termizi <ahmadsyafiq93@gmail.com>
2021-05-02 00:06:30 +0800

    Chore: Add test serializer validation

--- preggy/kicks/tests.py
+++ preggy/kicks/tests.py
@@ -13,7 +13,7 @@ class TestFromToDateSerializer(SimpleTestCase):
         """
         serializer = FromToDateSerializer(
             data={
-                "from_date": "01-01-2021",
+                "from_date": "2021-01-01",
             }
         )
         with self.assertRaises(serializers.ValidationError):
@@ -26,7 +26,34 @@ class TestFromToDateSerializer(SimpleTestCase):
         """
         serializer = FromToDateSerializer(
             data={
-                "to_date": "01-01-2021",
+                "to_date": "2021-01-01",
+            }
+        )
+        with self.assertRaises(serializers.ValidationError):
+            serializer.is_valid(raise_exception=True)
+
+    def test_serializer_valid(self):
+        """
+        FromToDateSerializer should not raise ValidationError if from_date and
+        to_date is provided
+        """
+        serializer = FromToDateSerializer(
+            data={
+                "to_date": "2021-01-31",
+                "from_date": "2021-01-31",
+            }
+        )
+        self.assertEquals(serializer.is_valid(raise_exception=True), True)
+
+    def test_serializer_invalid_format(self):
+        """
+        FromToDateSerializer should raise ValidationError if from_date and
+        to_date is not in 'YYYY-MM-DD' format
+        """
+        serializer = FromToDateSerializer(
+            data={
+                "to_date": "31-01-2021",
+                "from_date": "31-01-2021",
             }
         )
         with self.assertRaises(serializers.ValidationError):
